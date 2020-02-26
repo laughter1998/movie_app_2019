9b8b07605b4bf820d2f8c990cee74ba8384ee127
laughter1998 <laughter98@hanmail.net>
2020-02-26 16:30:05 +0900

    2월 27일

---
+++ src/gnb.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Gnb() {
+  return (
+    <div className="gnb">
+     <ul>
+      <li>1</li>
+      <li>2</li>
+     </ul>
+    </div>
+  );
+}
+
+
+
+export default Gnb;
---
+++ src/potato.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Potato() {
+  return (
+    <div className="gnb">
+     <ul>
+      <li>1</li>
+      <li>2</li>
+     </ul>
+    </div> 
+  );
+}
+
+
+
+export default Potato;
