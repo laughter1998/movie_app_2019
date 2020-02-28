8e26162d4963b8033d06f5b887f97896914ceca4
laughter1998 <laughter98@hanmail.net>
2020-02-28 17:17:01 +0900

    2월 28일 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
 import React from 'react';
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
 
 class App extends React.Component{
   state = {
-    isLoading : true
+    isLoading : true,
+    movies : []
   };
-  componentDidMount(){
-    setTimeout(() => {
-      this.setState({ isLoading: false});
-    }, 6000);
+
+  getMovies = async () => {
+    // const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    // console.log(movies.data.data.movies);
+    const {
+        data : {
+          data: { movies }
+        }
+      } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    console.log(movies);
+    this.setState({movies:movies , isLoading: false});
+  }
+  componentDidMount() {
+    this.getMovies();
   }
   render(){
-    const {isLoading}= this.state;
-    return <div>{isLoading ? "Loading" : "We are ready"}</div>
+    const {isLoading, movies}= this.state;
+    return (
+      <section>
+        {isLoading ?
+          <div className="loader"><span className="loader__text">loading...</span></div>
+          : movies.map(movie => {
+          // console.log(movie);
+          return (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+            />
+          );
+
+        })}
+      </section>
+  );
   }
 }
 // https://yts-proxy.now.sh/list_movies.json
